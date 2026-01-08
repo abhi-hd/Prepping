ca96cac1cd4f82d281d177e274fd05f26a38ebb2
AbhiHD <abhiiii.hd@gmail.com>
2026-01-08 12:22:59 +0530

    3

---
+++ 3_longestSubstringWithoutRepeatingCharacters.cpp
@@ -0,0 +1,33 @@
+/*
+Given a string s, find the length of the longest
+
+without duplicate characters.
+
+
+
+Example 1:
+
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3. Note that "bca" and "cab" are also correct answers.
+*/
+
+class Solution
+{
+public:
+    int lengthOfLongestSubstring(string s)
+    {
+        int result = 0, l = s.size(), lo = 0;
+        unordered_map<char, int> frequency;
+        for (int i = 0; i < l; i++)
+        {
+            frequency[s[i]]++;
+            while (frequency[s[i]] > 1)
+            {
+                frequency[s[lo++]]--;
+            }
+            result = max(i - lo + 1, result);
+        }
+        return result;
+    }
+};
