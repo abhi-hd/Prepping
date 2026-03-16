68e074aecb30703ebc0e8479c892322a88570d0c
AbhiHD <abhiiii.hd@gmail.com>
2026-03-16 08:31:47 +0530

    73

---
+++ 73_setMatrixZeroes-spaceOptimized.cpp
@@ -0,0 +1,58 @@
+/*
+Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
+
+You must do it in place.
+Example 1:
+
+Input: matrix = [[1,1,1],[1,0,1],[1,1,1]]
+Output: [[1,0,1],[0,0,0],[1,0,1]]
+
+Example 2:
+
+Input: matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    void setZeroes(vector<vector<int>> &matrix)
+    {
+        int n = matrix.size(), m = matrix[0].size();
+        bool firstRow = false, firstColumn = false;
+        for (int i = 0; i < n; i++)
+            if (matrix[i][0] == 0)
+                firstColumn = true;
+        for (int j = 0; j < m; j++)
+            if (matrix[0][j] == 0)
+                firstRow = true;
+        for (int i = 1; i < n; i++)
+        {
+            for (int j = 1; j < m; j++)
+            {
+                if (matrix[i][j] == 0)
+                {
+                    matrix[i][0] = matrix[0][j] = 0;
+                }
+            }
+        }
+        for (int i = 1; i < n; i++)
+        {
+            for (int j = 1; j < m; j++)
+            {
+                if (matrix[0][j] == 0 || matrix[i][0] == 0)
+                {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        if (firstRow)
+            for (int j = 0; j < m; j++)
+                matrix[0][j] = 0;
+        if (firstColumn)
+            for (int i = 0; i < n; i++)
+                matrix[i][0] = 0;
+    }
+};
